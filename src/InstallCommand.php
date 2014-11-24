2487b3663bba0f56bcc8f33b0b8c878618792588
Jason McCallister <themccallister@gmail.com>
2014-11-24 14:05:39 -0800

    Add InstallCommand

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -4,7 +4,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class InstallCommand extends Command {
 
@@ -18,15 +18,18 @@ class InstallCommand extends Command {
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
 		$helper = $this->getHelper('question');
-		$question = new ChoiceQuestion('Do you agree with the Craft terms and conditions (default is no)? ', array(
+		$question = new ChoiceQuestion('Do you agree with the Craft terms and conditions? (default is no)', array(
 			'yes','no'), 1);
 
 		$question->setErrorMessage('You must agree to the terms and conditions.');
 
 		$answer = $helper->ask($input, $output, $question);
 
-		if ($answer == 1) {
-			$output->writeln('hello!');
+		if ($answer == 0) {
+
+			// install Craft
+			$output->writeln('Lets install Craft!');
+
 		}
 	}
 }
