5a71cee792deb362bc964e0fd0627f39dfdc568b
Jason McCallister <themccallister@gmail.com>
2014-11-24 17:05:29 -0800

    Update and docblock

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -9,6 +9,11 @@ use Symfony\Component\Process\Process;
 
 class InstallCommand extends Command {
 
+	/**
+	* Configure the command options.
+	*
+	* @return void
+	*/
 	public function configure()
 	{
 		$this->setName('install')
@@ -16,12 +21,19 @@ class InstallCommand extends Command {
 			->addArgument('directory', InputArgument::REQUIRED, 'Target installation directory.');
 	}
 
+	/**
+	* Execute the command.
+	*
+	* @param  \Symfony\Component\Console\Input\InputInterface  $input
+	* @param  \Symfony\Component\Console\Output\OutputInterface  $output
+	* @return void
+	*/
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
 		$helper = $this->getHelper('question');
 
-		$question = new ChoiceQuestion('Do you agree with the Craft terms and conditions? (default is no)', array(
-			'yes','no'), 1);
+		$question = new ChoiceQuestion('Do you agree with the Craft terms and conditions? [NO]', array(
+			'Yes','No'), 1);
 
 		$question->setErrorMessage('You must agree to the terms and conditions.');
 
@@ -32,11 +44,11 @@ class InstallCommand extends Command {
 
 			$craftUrl = "http://buildwithcraft.com/latest.zip?accept_license=yes";
 
-			$process = new Process("curl -L -o craft.zip $craftUrl");
+			$downloadProcess = new Process("curl -L -o craft.zip $craftUrl");
 
-			$process->start();
+			$downloadProcess->start();
 
-			$process->wait(function ($type, $buffer)
+			$downloadProcess->wait(function ($type, $buffer)
 			{
 
 				if (Process::ERR === $type)
@@ -51,17 +63,40 @@ class InstallCommand extends Command {
 
 			});
 
-			// executes after the command finishes
-			if (!$process->isSuccessful())
+			if (!$downloadProcess->isSuccessful())
 			{
-				throw new \RuntimeException($process->getErrorOutput());
+				throw new \RuntimeException($downloadProcess->getErrorOutput());
 			};
 
-			$output->writeln($process->getOutput());
+			$output->writeln($downloadProcess->getOutput());
 
-			// TODO unzip the craft.zip
+			$directory = $input->getArgument('directory');
 
-			// TODO move to the installation directory
+			$unzipProcess = new Process("unzip craft.zip -d $directory");
+
+			$unzipProcess->start();
+
+			$unzipProcess->wait(function ($type, $buffer)
+			{
+
+				if (Process::ERR === $type)
+				{
+					echo $buffer;
+				}
+
+				else
+				{
+					$output->writeln($buffer);
+				}
+
+			});
+
+			if (!$unzipProcess->isSuccessful())
+			{
+				throw new \RuntimeException($unzipProcess->getErrorOutput());
+			};
+
+			$output->writeln($unzipProcess->getOutput());
 
 		}
 	}
