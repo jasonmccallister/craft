ed3df9bff1fc98372350391468a6f22da0c79069
Jason McCallister <themccallister@gmail.com>
2014-12-02 23:22:45 -0800

    Major cleanup and functionality

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -5,15 +5,20 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Question\ChoiceQuestion;
-use Symfony\Component\Process\Process;
+use GuzzleHttp\ClientInterface;
+use ZipArchive;
 
 class InstallCommand extends Command {
 
-	/**
-	* Configure the command options.
-	*
-	* @return void
-	*/
+	private $client;
+
+	public function __construct(ClientInterface $client)
+	{
+		$this->client = $client;
+
+		parent::__construct();
+	}
+
 	public function configure()
 	{
 		$this->setName('install')
@@ -21,84 +26,78 @@ class InstallCommand extends Command {
 			->addArgument('directory', InputArgument::REQUIRED, 'Target installation directory.');
 	}
 
-	/**
-	* Execute the command.
-	*
-	* @param  \Symfony\Component\Console\Input\InputInterface  $input
-	* @param  \Symfony\Component\Console\Output\OutputInterface  $output
-	* @return void
-	*/
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
 		$helper = $this->getHelper('question');
 
-		$question = new ChoiceQuestion('Do you agree with the Craft terms and conditions? [NO]', array(
+		$question = new ChoiceQuestion('Do you agree with the Craft terms and conditions [NO]?', array(
 			'Yes','No'), 1);
 
-		$question->setErrorMessage('You must agree to the terms and conditions.');
+		$question->setErrorMessage("<error>You must agree to the terms and conditions.</error>");
 
 		$answer = $helper->ask($input, $output, $question);
 
-		if ($answer == 0)
+		$directory = getcwd() . '/' . $input->getArgument('directory');
+
+		if ($answer == 1)
 		{
+			$output->writeln('<error>You must agree to the license to continue...');
 
-			$craftUrl = "http://buildwithcraft.com/latest.zip?accept_license=yes";
-
-			$downloadProcess = new Process("curl -L -o craft.zip $craftUrl");
-
-			$downloadProcess->start();
-
-			$downloadProcess->wait(function ($type, $buffer)
-			{
-
-				if (Process::ERR === $type)
-				{
-					echo $buffer;
-				}
-
-				else
-				{
-					$output->writeln($buffer);
-				}
-
-			});
-
-			if (!$downloadProcess->isSuccessful())
-			{
-				throw new \RuntimeException($downloadProcess->getErrorOutput());
-			};
-
-			$output->writeln($downloadProcess->getOutput());
-
-			$directory = $input->getArgument('directory');
-
-			$unzipProcess = new Process("unzip craft.zip -d $directory");
-
-			$unzipProcess->start();
-
-			$unzipProcess->wait(function ($type, $buffer)
-			{
-
-				if (Process::ERR === $type)
-				{
-					echo $buffer;
-				}
-
-				else
-				{
-					$output->writeln($buffer);
-				}
-
-			});
-
-			if (!$unzipProcess->isSuccessful())
-			{
-				throw new \RuntimeException($unzipProcess->getErrorOutput());
-			};
-
-			$output->writeln($unzipProcess->getOutput());
-
+			exit(1);
 		}
+
+		$this->doesDirectoryExist($directory, $output);
+
+		$this->download($zip = $this->createZip(), $output);
+
+		$this->unzip($zip, $directory);
+
+		$this->cleanUp($zip);
+
+		$output->writeln('<comment>Craft installed and ready to rock!!!</comment>');
+	}
+
+	protected function doesDirectoryExist($directory, OutputInterface $output)
+	{
+		if (is_dir($directory))
+		{
+			$output->writeln('<error>Whoops... directory already exists!</error>');
+
+			exit(1);
+		}
+	}
+
+	protected function createZip()
+	{
+		return getcwd() . '/craft_' . md5(time()) . '.zip';
+	}
+
+
+	protected function download($zip, $output)
+	{
+		$response = $this->client->get('http://buildwithcraft.com/latest.zip?accept_license=yes')->getBody();
+
+		file_put_contents($zip, $response);
+
+		$output->writeln("<info>Downloading Craft, please grab a coffee or tea while you wait...</info>");
+
+		return $this;
+	}
+
+	protected function unzip($zip, $directory)
+	{
+		$archive = new ZipArchive;
+
+		$archive->open($zip);
+		$archive->extractTo($directory);
+		$archive->close();
+
+		return $this;
+	}
+
+	protected function cleanUp($zip)
+	{
+		unlink($zip);
 	}
 
 }
