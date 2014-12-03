b3fe6b2dfd6fe4c2a0bbf6b29cde7bb73de25a0c
Jason McCallister <themccallister@gmail.com>
2014-12-03 00:18:48 -0800

    Display output before download

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -75,10 +75,10 @@ class InstallCommand extends Command {
 
 	protected function download($zip, $output)
 	{
+		$output->writeln("<info>Downloading Craft, please grab a coffee or tea while you wait...</info>");
+
 		$response = $this->client->get('http://buildwithcraft.com/latest.zip?accept_license=yes')->getBody();
 
-		$output->writeln("<info>Downloading Craft, please grab a coffee or tea while you wait...</info>");
-		
 		file_put_contents($zip, $response);
 
 		return $this;
